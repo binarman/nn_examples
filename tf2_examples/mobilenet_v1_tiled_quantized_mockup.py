9c52fc206a9106bca930138cfe88ae869d98fd4e
binarman <efimov.alexander@gmail.com>
2021-07-15 13:02:34 +0300

    fix arbitrary tiling

--- tf2_examples/mobilenet_v1_tiled_quantized_mockup.py
+++ tf2_examples/mobilenet_v1_tiled_quantized_mockup.py
@@ -24,14 +24,33 @@ def separable_conv_tiled(x, input_channels, output_channels, stride):
   x = tf.nn.relu6(x)
   return x
 
+def get_idx_image(input_size, output_size, filter_size, strides, idx):
+  padding = output_size*strides - input_size
+  image_begin = idx * strides - padding // 2
+  return (image_begin, image_begin + filter_size - 1)
+
+def get_tile_part1_image(low, high):
+  img1_low = get_idx_image(64, 32, 3, 2, low)
+  img2_low = get_idx_image(64, 64, 3, 1, img1_low[0])
+  img3_low = get_idx_image(128, 64, 3, 2, img2_low[0])
+
+  img1_high = get_idx_image(64, 32, 3, 2, high)
+  img2_high = get_idx_image(64, 64, 3, 1, img1_high[1])
+  img3_high = get_idx_image(128, 64, 3, 2, img2_high[1])
+  return (img3_low[0], img3_high[1])
+
 def tiled_part_1(x, w_tiles, h_tiles):
   input_conv_weights = (np.random.random((3, 3, 3, 8)) * 2 - 1).astype(np.float32)
 
   tile = []
   h_input = 128
   w_input = 128
-  h_tile_size = h_input//h_tiles
-  w_tile_size = w_input//w_tiles
+  h_output = 32
+  w_output = 32
+  assert h_input % h_tiles == 0
+  assert w_input % w_tiles == 0
+  h_tile_size = h_output//h_tiles
+  w_tile_size = w_output//w_tiles
   input_conv_weights = (np.random.random((3, 3, 3, 8)) * 2 - 1).astype(np.float32)
 
   input_channels_1 = 8
@@ -46,34 +65,39 @@ def tiled_part_1(x, w_tiles, h_tiles):
   depthwise_weights_2 = (np.random.random((3, 3, input_channels_2, 1)) * 2 - 1).astype(np.float32)
   pointwise_weights_2 = (np.random.random((1, 1, input_channels_2, output_channels_2)) * 2 - 1).astype(np.float32)
 
-  low_cummulative_padding = 4
-  high_cummulative_padding = 4
-
-
   for i in range(h_tiles):
     tile += [[]]
     for j in range(w_tiles):
-      left_bound = max(w_tile_size * j - low_cummulative_padding, 0)
-      right_bound = min(w_tile_size * (j + 1) + high_cummulative_padding, w_input)
-      top_bound = max(h_tile_size * i - low_cummulative_padding, 0)
-      bottom_bound = min(h_tile_size * (i + 1) + high_cummulative_padding, h_input)
 
-      y = tf.slice(x, (0, left_bound, top_bound, 0), (1, right_bound - left_bound, bottom_bound - top_bound, 3))
+      tile_top = i * h_tile_size
+      tile_bottom = (i+1) * h_tile_size - 1
+      tile_left = j *w_tile_size
+      tile_right = (j+1) * w_tile_size - 1
+
+      # inference origin of output tile
+      left_bound, right_bound = get_tile_part1_image(tile_left, tile_right)
+      top_bound, bottom_bound = get_tile_part1_image(tile_top, tile_bottom)
 
       left_pad = 0
       right_pad = 0
       top_pad = 0
       bottom_pad = 0
 
-      if j == 0:
-        left_pad = low_cummulative_padding
-      if j == w_tiles-1:
-        right_pad = high_cummulative_padding
+      if left_bound < 0:
+        left_pad = -left_bound
+        left_bound = 0
+      if right_bound >= w_input:
+        right_pad = right_bound - w_input + 1
+        right_bound = w_input - 1
 
-      if i == 0:
-        top_pad = low_cummulative_padding
-      if i == h_tiles-1:
-        bottom_pad = high_cummulative_padding
+      if top_bound < 0:
+        top_pad = - top_bound
+        top_bound = 0
+      if bottom_bound >= h_input:
+        bottom_pad = bottom_bound - h_input + 1
+        bottom_bound = h_input - 1
+
+      y = tf.slice(x, (0, top_bound, left_bound, 0), (1, bottom_bound - top_bound + 1, right_bound - left_bound + 1, 3))
 
       need_padding = left_pad != 0 or right_pad != 0 or top_pad != 0 or bottom_pad != 0
       if need_padding:
@@ -103,7 +127,7 @@ def tiled_part_1(x, w_tiles, h_tiles):
 
 
 def model_forward(x):
-  x = tiled_part_1(x, 2, 2)
+  x = tiled_part_1(x, 4, 2)
   x = separable_conv(x, 32, 64, 2)
   x = separable_conv(x, 64, 64, 1)
   x = separable_conv(x, 64, 128, 2)
