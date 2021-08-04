66d6949cb9cbec88a8e70e1558d48438be389630
Alexander Efimov <a.efimov@samsung.com>
2021-08-04 18:53:08 +0300

    add argmax

--- tf2_examples/arg_max.py
+++ tf2_examples/arg_max.py
@@ -4,10 +4,10 @@ import tensorflow as tf
 import numpy as np
 # Construct a basic model.
 
-f = tf.function(lambda x: tf.nn.relu6(x))
+f = tf.function(lambda x: tf.math.argmax(x))
 
 # Create the concrete function.
-x = tf.TensorSpec(shape=[1, 3], dtype=np.float32)
+x = tf.TensorSpec(shape=[1, 3], dtype=tf.float32)
 concrete_func = f.get_concrete_function(x)
 converter = tf.lite.TFLiteConverter.from_concrete_functions([concrete_func])
 tflite_model = converter.convert()
