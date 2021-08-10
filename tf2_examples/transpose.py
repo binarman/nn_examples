a2c511e84f821a7c367b87a6f6a9d8db396fea9c
binarman <efimov.alexander@gmail.com>
2021-08-10 16:43:13 +0300

    add transpose and improve cast

--- tf2_examples/transpose.py
+++ tf2_examples/transpose.py
@@ -4,10 +4,12 @@ import tensorflow as tf
 import numpy as np
 # Construct a basic model.
 
-f = tf.function(lambda x: tf.cast(tf.cast(x, tf.int32), tf.float32))
+perm = tf.constant((0,3,1,2), dtype=tf.int32)
+
+f = tf.function(lambda x: tf.transpose(x, perm))
 
 # Create the concrete function.
-x = tf.TensorSpec((3,3), dtype=tf.float32)
+x = tf.TensorSpec(shape=[1, 2, 3, 4], dtype=np.float32)
 concrete_func = f.get_concrete_function(x)
 converter = tf.lite.TFLiteConverter.from_concrete_functions([concrete_func])
 tflite_model = converter.convert()
