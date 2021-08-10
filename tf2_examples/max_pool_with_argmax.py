a6d4c2f16d849ea3ac925d844e89ffd3a941445e
binarman <efimov.alexander@gmail.com>
2021-08-10 18:55:54 +0300

    complex max_pool_with_argmax

--- tf2_examples/max_pool_with_argmax.py
+++ tf2_examples/max_pool_with_argmax.py
@@ -4,10 +4,10 @@ import tensorflow as tf
 import numpy as np
 # Construct a basic model.
 
-f = tf.function(lambda x: tf.nn.max_pool_with_argmax(x, ksize=3, strides=1, padding="VALID"))
+f = tf.function(lambda x: tf.nn.max_pool_with_argmax(x, ksize=3, strides=1, padding="SAME", output_dtype=tf.int32))
 
 # Create the concrete function.
-input_data = tf.constant(1., shape=[1, 4, 4, 1])
+input_data = tf.constant(1., shape=[1, 4, 4, 32])
 concrete_func = f.get_concrete_function(input_data)
 converter = tf.lite.TFLiteConverter.from_concrete_functions([concrete_func])
 
